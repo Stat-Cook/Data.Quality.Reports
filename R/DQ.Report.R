1f0fd9389c9897b0f6581cad2725294629e32162
Rob <robert.cook@bcu.ac.uk>
2022-04-25 16:14:01 +0100

    Implement DQ Report

---
+++ R/DQ.Report.R
@@ -0,0 +1,42 @@
+modal.size <- function(values){
+  modal.size <- table(values) %>% sort() %>% tail(1)
+  c(Value = names(modal.size),
+    Proportion = as.numeric(modal.size) / length(values))
+}
+
+
+DQ.Report <- function(con, pattern){
+  #' @export
+  missing <- c(NA, "", " ", "NA", "nan", "  /  /    ",
+               "NULL", "null", "Null")
+
+  tabs <- dbListTables(con)
+  tabs <- tabs[grepl(pattern, tabs)]
+
+  results <- list()
+
+  for (i in tabs){
+    data <- tbl(con, i) %>% collect()
+
+    data.modal.size <- lapply(data, modal.size)
+    data.modal.size <- t(do.call(data.frame, data.modal.size))
+    data.modal.size <- as_tibble(data.modal.size, rownames="Variable")
+    data.modal.size$Proportion <- as.numeric(data.modal.size$Proportion)
+    data.modal.size <- data.modal.size %>% filter(Proportion > 0.8)
+    data.missing <- data.modal  %>% filter(Value %in% missing)
+    data.modal <- data.modal  %>% filter(!(Value %in% missing))
+
+    last.col.string <- tail(colnames(data), 1)
+    last.col <- data[[last.col.string]]
+    comma.cnt <- str_count(last.col, ",")
+    comma.tbl <- table(comma.cnt)
+
+    results[[i]] <- list("Missing" = data.missing,
+                         "Modal" = data.modal,
+                         "Comma" = comma.tbl)
+  }
+}
+i <- "Boston"
+data.modal
+
+results[["Boston"]]
